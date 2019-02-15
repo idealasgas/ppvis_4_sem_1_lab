c68e21bcc451769b839cd6f489df1805534d820e
Marie Akhramenko <triangletrue@yahoo.com>
2019-02-15 11:22:07 +0300

    First task simply works

--- src/main.java
+++ src/main.java
@@ -1,16 +1,18 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class main extends Application implements EventHandler {
+public class main extends Application {
     Button button;
     TextField text_field;
 
@@ -20,24 +22,25 @@ public class main extends Application implements EventHandler {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         primaryStage.setTitle("лабораторная 1");
-        button = new Button("fuck");
-        text_field = new TextField("набери в меня текст");
+        button = new Button("кнопка");
+        text_field = new TextField();
         text_field.setPrefWidth(400);
-        button.setOnAction(this);
+        text_field.getText();
+        ComboBox combo_box = new ComboBox();
+
+
+        button.setOnAction((event -> {
+            combo_box.getItems().add(text_field.getText());
+        }));
         HBox layout = new HBox();
         layout.setPadding(new Insets(15, 12, 15, 12));
         layout.setSpacing(10);
-        layout.getChildren().addAll(text_field, button);
+        layout.getChildren().addAll(text_field, button, combo_box);
         Scene scene = new Scene(layout, 300, 300);
+
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    @Override
-    public void handle(Event event) {
-        if (event.getSource()==button){
-            System.out.println("hey");
-        }
     }
 }
