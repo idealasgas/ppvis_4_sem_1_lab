e0fc1a434ffce8489744f52911a9c768e546bef3
Marie Akhramenko <triangletrue@yahoo.com>
2019-02-18 23:39:42 +0300

    Still 5th task

--- src/FifthTask.java
+++ src/FifthTask.java
@@ -1,7 +1,9 @@
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -13,18 +15,33 @@ public class FifthTask {
 
     public VBox getBox(){
         TableView table = new TableView();
-        TableColumn firstColumn = new TableColumn();
-        TableColumn secondColumn = new TableColumn();
-        table.getColumns().addAll(firstColumn, secondColumn);
-// сделать чтобы текст из поля для текста удалялся
-        firstButton.setOnAction((event -> {
-            firstColumn.setText(textField.getText());
-        }));
 
-        secondButton.setOnAction((event -> {
-            secondColumn.setText(firstColumn.getText());
-            firstColumn.setText("");
+        TableColumn firstColumn = new TableColumn("First column");
+        firstColumn.setMinWidth(150);
+        firstColumn.setCellValueFactory(
+                new PropertyValueFactory<>("string"));
+
+        TableColumn secondColumn = new TableColumn("Second column");
+        secondColumn.setMinWidth(150);
+        firstColumn.setCellValueFactory(new PropertyValueFactory<>("string"));
+
+        ObservableList<Property> data = FXCollections.observableArrayList();
+
+        //table.setItems(data);
+
+        table.getColumns().addAll(firstColumn, secondColumn);
+        table.setPrefWidth(300);
+        // сделать чтобы текст из поля для текста удалялся
+        firstButton.setOnAction((event -> {
+            //firstColumn.setText(textField.getText());
+            data.add(new Property(textField.getText()));
+            table.setItems(data);
         }));
+//
+//        secondButton.setOnAction((event -> {
+//            secondColumn.setText(firstColumn.getText());
+//            firstColumn.setText("");
+//        }));
 
         HBox firstContainer = new HBox();
         firstContainer.getChildren().addAll(textField, firstButton, secondButton, thirdButton);
---
+++ src/Property.java
@@ -0,0 +1,11 @@
+public class Property {
+    public String string;
+
+    public String getString(){
+        return string;
+    }
+
+    Property(String value){
+        string = value;
+    }
+}
